b48c3927ad97a959885ee7ec22f41f7fed0cb22e
Sérgio Baixo <sergiobx2@gmail.com>
2022-10-15 18:30:22 +0100

    just printed
    printed email

--- source/main.cpp
+++ source/main.cpp
@@ -8,6 +8,9 @@ int main(){
 
     vector <int> vec;
     vec.push_back(23);
+    cout << "Email: sergiobx2@gmail.com" << endl;
+    
+    
     
 
     cout << "Hello World sid by Sergio Baixo" << endl;
